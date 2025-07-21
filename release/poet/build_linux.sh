a127db338902fafc51617d13f94255e783f63471
ElonJunior <ElonWang2@outlook.com>
2025-07-21 09:57:02 +0800

    add uTLS

--- release/poet/build_linux.sh
+++ release/poet/build_linux.sh
@@ -11,24 +11,8 @@ PROGRAME_NAME="sing-poet"
 outfile="sing-box"
 outfile1="${CURRENT_DIR}/test/${PROGRAME_NAME}"
 
-# check tuic service bug
-# GOMODIR=$(go env GOMODCACHE)
-# bugPkg="${GOMODIR}/github.com/sagernet/sing-quic"
-# srcPkgPtah=$(ls -td "${bugPkg}"@* | head -n 1)
-# bugStr=$(grep "authentication: unknown user" $srcPkgPtah/tuic/service.go | grep -v string | grep UUID)
-# if [[ -n $bugStr ]]; then
-#     echo "${srcPkgPtah}/tuic/service.go"
-#     echo -e "\n\tquic src code has bug, please check!!!"
-#     exit 1
-# fi
-
-
-
-
-
 # packaging...
 rm $outfile $outfile1
-#CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -trimpath -ldflags="-s -w -buildid=" -tags with_gvisor,with_dhcp,with_quic,with_acme,with_v2ray_api -o $outfile ./cmd/sing-box
 # 有需要则修改 Makefile::TAGS_POET2
 make poet_build
 chmod +x $outfile
